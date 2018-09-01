78e5e9595e206915c5bedf5cdcca1b18ce1fb022
Mark Sholund <mdsholund@gmail.com>
2018-09-01 12:34:38 -0400

    Add queries playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,39 @@
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('../server/models/Todo');
+const {User} = require('../server/models/User');
+const {ObjectID} = require('mongodb');
+
+// var id = '5b8aba173e61371ff81c5d86';
+
+// if (!ObjectID.isValid(id)) {
+//     console.log("ID not valid");
+// }
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log("Todo", todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//     if (!todo) {
+//         return console.log("Id not found");
+//     }
+//     console.log("Todo By Id", todo);
+// }).catch((e) => console.log(e));
+
+var id = '5b8abe1f0a0efa14f832ef55';
+User.findById(id).then((user) => {
+    if (!user) {
+        return console.log(`Unable to find user`);
+    }
+    console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+    console.log(e);
+});
